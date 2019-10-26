7363bc422eb9472c436556fbeff003ad2b9a6fdd
dkozak <dkozak94@gmail.com>
2019-10-26 17:59:46 +0200

    cors settings

---
+++ src/main/kotlin/cz/esn/fiesta/fiestapluginspringkotlin/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package cz.esn.fiesta.fiestapluginspringkotlin.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*", "http://localhost:3000", "https://fiesta-plugin-react.netlify.com/")
+                        .allowCredentials(true)
+            }
+        }
+
+    }
+}
